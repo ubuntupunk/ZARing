9370c770f492bf62fbfbd26adaf994c0bb38e1b0
DavDev Yes <ubuntupunk@users.noreply.github.com>
2022-09-14 07:33:19 +0200

    Update onionring-variables.js

--- onionring-variables.js
+++ onionring-variables.js
@@ -7,24 +7,24 @@
 
 //the full URLs of all the sites in the ring
 var sites = [
-'https://example.com/',
-'https://example.com/',
-'https://example.com/',
-'https://example.com/'
+'https://medialternatives.com/',
+'https://medialternatives.com/wiki/',
+'https://netbones.co.za/',
+'https://muizenmesh.co.za/'
 ];
 
 //the name of the ring
-var ringName = 'My Ring';
+var ringName = 'ZARing';
 
 /* the unique ID of the widget. two things to note:
  1) make sure there are no spaces in it - use dashes or underscores if you must
  2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value!*/
-var ringID = 'my-ring';
+var ringID = 'zar-ring';
 
 //should the widget include a link to an index page?
 var useIndex = false;
 //the full URL of the index page. if you're not using one, you don't have to specify anything here
-var indexPage = 'https://example.com/index.html';
+var indexPage = 'https://medialternatives.com/wiki/zaring/index.html';
 
 //should the widget include a random button?
 var useRandom = true;
